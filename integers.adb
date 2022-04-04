7d82c6c34476a8d165c1a228f5de18b59768d606
thejbte <thejbte@gmail.com>
2022-04-04 15:10:53 -0500

    example

---
+++ integers.adb
@@ -0,0 +1,8 @@
+-- gnatclean integers.adb && gnatmake integers.adb 
+-- this program for cretae a integer variable
+
+procedure integers is 
+    someInt : Integer := 44;
+begin
+    someInt := 10;
+end integers;
