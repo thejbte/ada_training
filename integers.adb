c1c83cec225f5f497e31d13fa3a11e95cc9d2004
thejbte <thejbte@gmail.com>
2022-04-04 15:34:51 -0500

    example

--- integers.adb
+++ integers.adb
@@ -1,8 +1,19 @@
 -- gnatclean integers.adb && gnatmake integers.adb 
+-- gnatmake -gnatwe integers.adb   // WARINGNS AS ERROR
 -- this program for cretae a integer variable
+
+with Ada.Text_IO;   -- import or include
 
 procedure integers is 
     someInt : Integer := 44;
+    someNatural :   natural := 0;
+    somePostive :   positive := 1;  -- case sensitive  Positive or PoSiTiVe
 begin
     someInt := 10;
+    Ada.Text_IO.Put_Line("This is a Integer:" & Integer'Image(someInt));
+
+    -- limits
+    Ada.Text_IO.Put_Line("Integer  [" & Integer'Image(Integer'First)  & "] to [" & Integer'Image(Integer'lAST) &"]" );
+    Ada.Text_IO.Put_Line("Natural  [" & Natural'Image(Natural'First)  & "] to [" & Natural'Image(Natural'lAST) &"]" );
+    Ada.Text_IO.Put_Line("Positive [" & Positive'Image(Positive'First)  & "] to [" & Positive'Image(Positive'lAST) &"]" );
 end integers;
